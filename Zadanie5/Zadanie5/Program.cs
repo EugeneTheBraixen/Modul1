2b5c6d46ad76200f3fa2e97d47359e503b71a1d0
EugeneTheBraixen <100828745+EugeneTheBraixen@users.noreply.github.com>
2023-09-30 11:02:53 +0300

    Add files via upload

---
+++ Zadanie5/Zadanie5/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Zadanie5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите возраст:");
+            string input = Console.ReadLine();
+
+            if (double.TryParse(input, out double age) && age >= 0)
+            {
+                if (age >= 18)
+                {
+                    Console.WriteLine("Вы можете получить права");
+                }
+                else
+                {
+                    Console.WriteLine("Вы не можете получить права");
+                }
+            }
+            else if (age < 0)
+            {
+                Console.WriteLine("Ошибка! Возраст не может быть отрицательным");
+            }
+            else
+            {
+                Console.WriteLine("Ошибка! Введите корректное значение");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
