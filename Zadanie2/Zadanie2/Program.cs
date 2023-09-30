6da4ffa68b9364243443d03b29c6a26534ddb479
EugeneTheBraixen <100828745+EugeneTheBraixen@users.noreply.github.com>
2023-09-30 11:00:04 +0300

    Add files via upload

---
+++ Zadanie2/Zadanie2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Zadanie2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите радиус круга: ");
+            string input = Console.ReadLine();
+
+            if (double.TryParse(input, out double rad) && rad >= 0)
+            {
+                double square = Math.PI * Math.Pow(rad, 2);
+                Console.WriteLine($"Площадь окружности равна {square}");
+            }
+            else
+            {
+                Console.WriteLine("Ошибка! Введенное значение не соответствует условию.");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
