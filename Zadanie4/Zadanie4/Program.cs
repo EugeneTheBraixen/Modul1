5a2465ebf4cfe9bcaa90d911e21b6d0ded9cb767
EugeneTheBraixen <100828745+EugeneTheBraixen@users.noreply.github.com>
2023-09-30 11:01:41 +0300

    Add files via upload

---
+++ Zadanie4/Zadanie4/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Zadanie3
+{
+    internal class Programm
+    {
+        static void Main(string[] args)
+        {
+            Random RandomMassive = new Random();
+            int[] massive = new int[20];
+            for (int i = 0; i < massive.Length; i++)
+            {
+                massive[i] = RandomMassive.Next(1, 101);
+            }
+            int max = massive[0];
+            int min = massive[0];
+            foreach (int num in massive)
+            {
+                max = Math.Max(max, num);
+                min = Math.Min(min, num);
+            }
+            Console.WriteLine("Массив случайных чисел:\n" + string.Join(" ", massive));
+            Console.WriteLine($"Максимальное значение: {max}");
+            Console.WriteLine($"Минимальное значение: {min}");
+            Console.ReadLine();
+        }
+    }
+}
